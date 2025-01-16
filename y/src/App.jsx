fff67729df9c33097f6565b89a29041644360c12
LIGHT-25 <rupakchatterjee1998@gmail.com>
2025-01-16 15:40:25 +0530

    first commit

---
+++ y/src/App.jsx
@@ -0,0 +1,14 @@
+import TODO from "./components/TODO";
+import TodoItems from "./components/TodoItems";
+
+const App = () => {
+  return (
+    <div className="bg-black bg-opacity-80 grid min-h-screen py-4">
+      <div>
+        <TODO />
+      </div>
+    </div>
+  );
+};
+
+export default App;
